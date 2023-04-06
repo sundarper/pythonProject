567a6fc6f8aff8fdb7e546ea42e0cc12374177b7
sunda <sundarper@gmail.com>
2023-04-06 14:31:00 -0400

    My first print

---
+++ 02printFunction.py
@@ -0,0 +1,18 @@
+varString = "My First Line which is split in multiple line"
+print (varString)
+
+varString = "My First Line which is \n split in multiple line"
+print (varString)
+
+varString = "My First Line which is \\n split in multiple line"
+print (varString)
+
+varString2 = ", this is my Second Line"
+print (varString,varString2)
+print (varString+varString2)
+varString3 = 3
+print (varString+varString2+str(varString3))
+print (varString,varString2,varString3)
+print (type(varString3))
+print (type(varString2))
+
