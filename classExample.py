601f0437b8e2182e3fb9927d91e1e72a7af5913d
sunda <sundarper@gmail.com>
2023-04-12 14:56:01 -0400

    first python class

--- classExample.py
+++ classExample.py
@@ -1,14 +1,25 @@
 
 class person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, city):
         self.name = name
         self.age = age
+        self.city = city
 
     def myFunction(self):
         print("My name is :", self.name)
         print("My Age is :", self.age)
 
-p1 = person("Sundar Rajan", "40")
+    def myCity(self):
+        print("My Place : ", self.city)
 
+# name = input("Enter your name : ")
+# age = int(input("Enter your age : "))
+
+p1 = person("Sundar Rajan", "40", "Fairfax")
+#p1 = person(name, age)
 p1.myFunction()
+p1.myCity()
+
+
+
 
